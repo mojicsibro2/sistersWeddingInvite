620e7cc85dc95e2b2adcff7f534f3bb394333cba
ABDULLAHI OMOWUMI IBRAHEEM <71957195+mojicsibro2@users.noreply.github.com>
2024-02-22 15:37:45 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function myFuncClose() {
+    document.getElementById('bugger').style.display = 'none';
+    // document.getElementById('card').style.top = `180%`;
+    document.getElementById('open').style.display = 'flex';
+    document.getElementById('close').style.display = 'none';
+}
+function myFuncOpen() {
+    document.getElementById('bugger').style.display = 'flex';
+    // document.getElementById('card').style.top = `110%`;
+    document.getElementById('open').style.display = 'none';
+    document.getElementById('close').style.display = 'flex';
+}
